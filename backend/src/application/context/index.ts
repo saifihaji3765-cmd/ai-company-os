23890626ed5b889dee09470414e1c7ce25de1504
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-08-14 10:38:12 +0530

    Add index.ts for application context exports

---
+++ backend/src/application/context/index.ts
@@ -0,0 +1,12 @@
+export type {
+  ApplicationContext,
+  ApplicationContextSource,
+} from "./application-context.types";
+
+export {
+  createApplicationContext,
+} from "./application-context.factory";
+
+export type {
+  CreateApplicationContextInput,
+} from "./application-context.factory";
