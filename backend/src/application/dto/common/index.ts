fd9bdb53ebebbd3e652746226ab546ded82d9ebf
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-08-14 10:52:16 +0530

    Create index.ts to export DTO types

---
+++ backend/src/application/dto/common/index.ts
@@ -0,0 +1,11 @@
+export type {
+  PaginationDto,
+  PaginationRequestDto,
+} from "./pagination.dto";
+
+export type {
+  ErrorDto,
+  PaginatedDto,
+  PaginationMetaDto,
+  SuccessDto,
+} from "./result.dto";
