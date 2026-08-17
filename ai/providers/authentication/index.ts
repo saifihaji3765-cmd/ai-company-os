f3ba4a8c64726b0d9e4fc28f4b01148ccfec68b8
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-08-17 10:48:41 +0530

    Create index.ts

---
+++ ai/providers/authentication/index.ts
@@ -0,0 +1,12 @@
+export type {
+  AIProviderCredentials,
+  AIProviderCredentialSource,
+} from "./provider-credentials.types";
+
+export type {
+  AICredentialResolver,
+} from "./credential-resolver";
+
+export {
+  EnvironmentCredentialResolver,
+} from "./credential-resolver";
