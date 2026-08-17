8eec20bfb5550dcd2a19c1b92d568d9e8350de0f
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-08-17 05:50:12 +0530

    Add index.ts to export routing types and router

---
+++ ai/providers/routing/index.ts
@@ -0,0 +1,12 @@
+export type {
+  AIModelRouteRequest,
+  AIModelRoute,
+} from "./model-routing.types";
+
+export type {
+  AIRoutingPolicy,
+} from "./routing-policy";
+
+export {
+  AIModelRouter,
+} from "./model-router";
