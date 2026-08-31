72c28f94caa278420ccb916c03cba3482f3fe262
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-08-31 08:51:44 +0530

    Create index.ts for learning module exports

---
+++ ai /learning/index.ts
@@ -0,0 +1,12 @@
+export type {
+  AILearning,
+  AILearningType,
+} from "./learning.types";
+
+export type {
+  AILearningEngine,
+} from "./learning.engine";
+
+export {
+  BasicLearningEngine,
+} from "./learning.engine";
