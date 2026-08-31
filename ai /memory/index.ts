c076c2b415c1712ab42da34a9235f957951b38d8
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-08-31 10:28:57 +0530

    Create index.ts for memory module exports

---
+++ ai /memory/index.ts
@@ -0,0 +1,46 @@
+export type {
+  AIMemory,
+  AIMemoryType,
+  AIMemoryImportance,
+} from "./memory.types";
+
+export type {
+  AIMemoryStore,
+} from "./memory.store";
+
+export {
+  InMemoryStore,
+} from "./memory.store";
+
+export type {
+  AIMemoryClassifier,
+} from "./memory.classifier";
+
+export {
+  BasicMemoryClassifier,
+} from "./memory.classifier";
+
+export type {
+  AIMemoryRelevanceEngine,
+} from "./memory.relevance";
+
+export {
+  BasicMemoryRelevanceEngine,
+} from "./memory.relevance";
+
+export type {
+  AIMemoryValidationResult,
+  AIMemoryValidator,
+} from "./memory.validator";
+
+export {
+  BasicMemoryValidator,
+} from "./memory.validator";
+
+export type {
+  AIMemoryIndex,
+} from "./memory.index";
+
+export {
+  BasicMemoryIndex,
+} from "./memory.index";
