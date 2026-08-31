a7134ebd0e916063f2a372212c7c66da493e4091
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-08-31 08:49:45 +0530

    Create index.ts for validation exports

---
+++ ai /validation/index.ts
@@ -0,0 +1,13 @@
+export type {
+  AIValidationStatus,
+  AIValidationInput,
+  AIValidationResult,
+} from "./validation.types";
+
+export type {
+  AIValidationEngine,
+} from "./validation.engine";
+
+export {
+  BasicValidationEngine,
+} from "./validation.engine";
