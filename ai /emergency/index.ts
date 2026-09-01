4966b9d64845641fda0d77315407833d4ff886d6
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-09-01 09:10:02 +0530

    Create index.ts for emergency module exports

---
+++ ai /emergency/index.ts
@@ -0,0 +1,13 @@
+export type {
+  AIEmergencySeverity,
+  AIEmergencyStatus,
+  AIEmergencyEvent,
+} from "./emergency.types";
+
+export type {
+  AIEmergencyManager,
+} from "./emergency.manager";
+
+export {
+  BasicEmergencyManager,
+} from "./emergency.manager";
